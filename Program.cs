73e6a8d50c63c3e69d7057b889d93d138938d3b2
TheSwerik <derswerik@gmail.com>
2020-08-20 15:14:40 +0200

    FIXED CORS HELL YEAH WOHOO

--- Program.cs
+++ Program.cs
@@ -14,7 +14,13 @@ namespace UI
             builder.RootComponents.Add<App>("app");
 
             builder.Services.AddScoped(
-                sp => new HttpClient {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
+                sp =>
+                {
+                    return new HttpClient
+                           {
+                               BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+                           };
+                });
 
             await builder.Build().RunAsync();
         }
