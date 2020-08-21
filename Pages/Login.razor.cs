993237246329e0f1bcbf1e4264a933e64a5a9ff1
TheSwerik <derswerik@gmail.com>
2020-08-21 16:20:24 +0200

    cleanup

--- Pages/Login.razor.cs
+++ Pages/Login.razor.cs
@@ -1,15 +1,17 @@
-﻿using System;
+﻿// ReSharper disable FieldCanBeMadeReadOnly.Local
+// ReSharper disable ConvertToConstant.Local
+// ReSharper disable UnusedAutoPropertyAccessor.Local
+
+using System;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using UI.Services;
 
-namespace UI.Pages.Login
+namespace UI.Pages
 {
     public partial class Login
     {
-        // ReSharper disable once FieldCanBeMadeReadOnly.Local
         private string _userName = "";
-
         [Inject] private UserService UserService { get; set; }
 
         private void CheckEnter(KeyboardEventArgs e)
---
+++ Pages/ServerBrowser.razor.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace UI.Pages
+{
+    public partial class ServerBrowser
+    {
+        private void Refresh() { Console.WriteLine(UserService.CurrentUser); }
+    }
+}
