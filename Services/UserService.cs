54869061e101d542e4b8cee3f33c8237059196f7
TheSwerik <derswerik@gmail.com>
2020-08-21 14:33:09 +0200

    cleanup

--- Services/UserService.cs
+++ Services/UserService.cs
@@ -1,29 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Net.Http;
-using System.Net.Http.Json;
-using System.Text;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Components;
 using UI.Entities;
 
 namespace UI.Services
 {
-    public class UserService 
+    public class UserService
     {
-        public User CurrentUser;
-        public UserService(HttpClient http) { Http = http; }
-        [Inject] private HttpClient Http { get; set; }
         private const string Url = "user";
+        private readonly HttpClient _http;
+        public User CurrentUser;
+        public UserService(HttpClient http) { _http = http; }
 
         public async Task<User> CreateUser(string username)
         {
-            var response = await Http.PostAsync(Url, new StringContent("abc"));
+            var response = await _http.PostAsync(Url, new StringContent("abc"));
             return CurrentUser;
         }
+
         public async Task<IEnumerable<User>> GetAllUsers()
         {
-            var response = await Http.GetAsync( Url);
+            var response = await _http.GetAsync(Url);
             response.EnsureSuccessStatusCode();
             Console.WriteLine(await response.Content.ReadAsStringAsync());
             return null;
