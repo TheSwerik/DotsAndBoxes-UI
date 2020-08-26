15d1e0db889830f104b3f0d9eaa857f5e149b156
Swerik <derswerik@gmail.com>
2020-08-26 11:51:07 +0200

    add Navbar

---
+++ Shared/Navbar.razor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+
+namespace UI.Shared
+{
+    public partial class Navbar
+    {
+        private string _user = "swerik";
+        [Inject] public NavigationManager MyNavigationManager { get; set; }
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            Console.WriteLine(MyNavigationManager.Uri);
+        }
+    }
+}
