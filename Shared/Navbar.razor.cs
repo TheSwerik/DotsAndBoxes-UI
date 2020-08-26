127ecd4960ea918bdf2a1d3dea2e31cdf920491a
Swerik <derswerik@gmail.com>
2020-08-26 12:29:57 +0200

    fix active item in Navbar

--- Shared/Navbar.razor.cs
+++ Shared/Navbar.razor.cs
@@ -13,5 +13,20 @@ namespace UI.Shared
             base.OnInitialized();
             Console.WriteLine(MyNavigationManager.Uri);
         }
+
+        private bool isHighscore()
+        {
+            return MyNavigationManager.Uri.Equals("highscore", StringComparison.InvariantCultureIgnoreCase);
+        }
+
+        private bool isLobby()
+        {
+            return MyNavigationManager.Uri.Contains("lobby", StringComparison.InvariantCultureIgnoreCase);
+        }
+
+        private bool isLogin()
+        {
+            return MyNavigationManager.Uri.Equals("login", StringComparison.InvariantCultureIgnoreCase);
+        }
     }
 }
