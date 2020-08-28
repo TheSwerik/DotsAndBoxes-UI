3a32fa8957e2840c25e6ad6063ac531771c39eee
Swerik <derswerik@gmail.com>
2020-08-28 14:07:48 +0200

    AutoRedirect

--- Pages/LobbyBrowser.razor.cs
+++ Pages/LobbyBrowser.razor.cs
@@ -1,6 +1,16 @@
-﻿namespace UI.Pages
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using UI.Services;
+using UI.Services.Model;
+
+namespace UI.Pages
 {
     public partial class LobbyBrowser
     {
+        private IEnumerable<User> _users = new List<User>();
+        [Inject] private UserService UserService { get; set; }
+
+        protected override async Task OnInitializedAsync() { _users = await UserService.GetAllUsers(); }
     }
 }
--- Shared/Navbar.razor.cs
+++ Shared/Navbar.razor.cs
@@ -1,5 +1,6 @@
-﻿using System;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
 using UI.Services;
 using UI.Util;
 
@@ -9,14 +10,23 @@ namespace UI.Shared
     {
         private const string RegisterUri = "/register";
         private const string LoginUri = "/login";
+        [Inject] private AuthenticationStateProvider AuthenticationStateProvider { get; set; }
         [Inject] private NavigationManager NavigationManager { get; set; }
         [Inject] private UserService UserService { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            var authState = await AuthenticationStateProvider.GetAuthenticationStateAsync();
+            if (!authState.User.Identity.IsAuthenticated && !IsLogin() && !IsRegister())
+                NavigationManager.NavigateTo(LoginUri);
+        }
 
         private bool IsHighscore() { return Comparer.EqualsUri(NavigationManager, "highscore"); }
 
         private bool IsLobby() { return Comparer.ContainsUri(NavigationManager, "lobby"); }
 
         private bool IsLogin() { return Comparer.EqualsUri(NavigationManager, "login", ""); }
+        private bool IsRegister() { return Comparer.EqualsUri(NavigationManager, "register", ""); }
 
         private void NavigateTo(string uri) { NavigationManager.NavigateTo(uri, true); }
     }
