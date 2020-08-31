17d21d8988d111699cd41b29faadf8a6adf7525c
Swerik <derswerik@gmail.com>
2020-08-31 12:44:02 +0200

    CLeanup Pages

--- Pages/Game.razor.cs
+++ Pages/Game.razor.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using Microsoft.AspNetCore.Components;
+﻿using Microsoft.AspNetCore.Components;
 
 namespace UI.Pages
 {
     public partial class Game
     {
         [Parameter] public int GameId { get; set; }
-
-        protected override void OnInitialized() { Console.WriteLine($"Browser: {GameId}"); }
     }
 }
