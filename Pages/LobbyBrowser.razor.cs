219335a9b26c81bf7367fdbc72435d6650605060
Swerik <derswerik@gmail.com>
2020-08-31 12:01:34 +0200

    cleanup

--- Pages/LobbyBrowser.razor.cs
+++ Pages/LobbyBrowser.razor.cs
@@ -1,16 +1,6 @@
-﻿using System.Collections.Generic;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Components;
-using UI.Services;
-using UI.Services.Model;
-
-namespace UI.Pages
+﻿namespace UI.Pages
 {
     public partial class LobbyBrowser
     {
-        private IEnumerable<User> _users = new List<User>();
-        [Inject] private UserService UserService { get; set; }
-
-        protected override async Task OnInitializedAsync() { _users = await UserService.GetAllUsers(); }
     }
 }
