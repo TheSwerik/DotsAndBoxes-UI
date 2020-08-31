86621cda60b44ac46b112fd4a78d2f4580fa4c54
gebauer <Dennis.Gebauer@adesso.de>
2020-08-31 07:58:38 +0200

    added join button

--- Pages/LobbyBrowser.razor.cs
+++ Pages/LobbyBrowser.razor.cs
@@ -4,6 +4,14 @@ namespace UI.Pages
 {
     public partial class LobbyBrowser
     {
-        private void Refresh() { Console.WriteLine(UserService.CurrentUser); }
+        private void Refresh()
+        {
+            Console.WriteLine(UserService.CurrentUser);
+        }
+
+        private async void JoinLobby(int lobby)
+        {
+            NavigationManager.NavigateTo("/lobby/ " + lobby);
+        }
     }
 }
