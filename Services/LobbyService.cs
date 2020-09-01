248d8061319b286151913d301055426f2f109e3e
gebauer <Dennis.Gebauer@adesso.de>
2020-09-01 16:58:06 +0200

    added LobbyService

---
+++ Services/LobbyService.cs
@@ -0,0 +1,22 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using UI.Services.Model;
+
+namespace UI.Services
+{
+    public class LobbyService : Service
+    {
+        public LobbyService(AuthenticationStateProvider authenticationStateProvider, HttpClient http,
+            NavigationManager navigationManager)
+            : base(authenticationStateProvider, http, navigationManager)
+        {
+        }
+
+        public async Task<LobbyDTO> CreateLobby(CreateLobbyDTO dto)
+        {
+            
+        }
+    }
+}
