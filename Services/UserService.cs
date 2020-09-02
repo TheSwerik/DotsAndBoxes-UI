647ad6c70e1e4a7ef5ac09c001d44a6fe61ffe36
TheSwerik <derswerik@gmail.com>
2020-09-02 15:35:20 +0200

    add more exceptions

--- Services/UserService.cs
+++ Services/UserService.cs
@@ -37,6 +37,7 @@ namespace UI.Services
             if (response.IsSuccessStatusCode) return await response.Content.ReadFromJsonAsync<User>();
             if (response.StatusCode == HttpStatusCode.NotFound)
                 Console.WriteLine($"USER WITH USERNAME {username} NOT FOUND!");
+            Console.WriteLine(await response.Content.ReadAsStringAsync());
             return null;
         }
     }
